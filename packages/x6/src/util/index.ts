c2d5523f319fac0fe64286e0e0dcbdf06e9c77d3
qhook <140258216+qhook@users.noreply.github.com>
2025-01-10 16:51:19 +0800

    make another change

--- packages/x6/src/util/index.ts
+++ packages/x6/src/util/index.ts
@@ -407,12 +407,15 @@ export namespace Util {
     const shape = toGeometryShape(node)
     const bbox = shape.bbox() || Rectangle.create()
 
-    // const transform = node.getAttribute('transform')
-    // if (transform) {
-    //   const nodeMatrix = Dom.transformStringToMatrix(transform)
-    //   return transformRectangle(bbox, nodeMatrix)
-    // }
+    const transform = node.getAttribute('transform')
+    if (transform) {
+      const nodeMatrix = Dom.transformStringToMatrix(transform)
+      return transformRectangle(bbox, nodeMatrix)
+    }
 
     return bbox
   }
 }
+
+
+export const random = () => Math.random().toString(32).slice(2)
